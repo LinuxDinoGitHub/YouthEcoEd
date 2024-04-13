c291f7ec0b594740a3ab3e380c5940d2194dc915
LinuxDinoGitHub <95269147+LinuxDinoGitHub@users.noreply.github.com>
2024-04-13 23:50:58 +0800

    End of April 13, Unfinished navbar

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function showNavbar(){
-    alert("yay");
+    let navbar = document.getElementById("mobile");
+    if (navbar.className === "hidden-mobile") {
+      navbar.className += " responsive";
+    } else {
+      navbar.className = "hidden-mobile";
+    }
 };
