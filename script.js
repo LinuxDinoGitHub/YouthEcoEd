2da91904a03dc76929fc58ead2dfa391e761d245
LinuxDinoGitHub <95269147+LinuxDinoGitHub@users.noreply.github.com>
2024-04-14 20:56:29 +0800

    New hamburger icon

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-function showNavbar(){
-    let navbar = document.getElementById("mobile");
-    if (navbar.className === "hidden-mobile") {
-      navbar.className = "show-mobile";
-    } else {
-      navbar.className = "hidden-mobile";
-    }
-};
+function openMenu(el){
+  el.classList.toggle("change");
+  let navbar = document.getElementById("mobile");
+  if (navbar.className === "hidden-mobile") {
+    navbar.className = "show-mobile";
+  } else {
+    navbar.className = "hidden-mobile";
+  }
+}
